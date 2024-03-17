ee5f7f28b317b9bc22231a22c72ac4b0e73a2c48
TheKodeToad <TheKodeToad@proton.me>
2024-03-17 01:26:35 +0000

    Fix messsage resolution

--- src/utils/resolve_message.rs
+++ src/utils/resolve_message.rs
@@ -4,8 +4,8 @@ use eyre::{eyre, Context as _, Result};
 use log::{debug, trace};
 use once_cell::sync::Lazy;
 use poise::serenity_prelude::{
-	ChannelId, ChannelType, Colour, Context, CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter,
-	Message, MessageId,
+	Channel, ChannelId, ChannelType, Colour, Context, CreateEmbed, CreateEmbedAuthor,
+	CreateEmbedFooter, Message, MessageId, Permissions,
 };
 use regex::Regex;
 
@@ -26,55 +26,80 @@ fn find_first_image(msg: &Message) -> Option<String> {
 }
 
 pub async fn resolve(ctx: &Context, msg: &Message) -> Result<Vec<CreateEmbed>> {
+	let Some(source_server_id) = msg.guild_id else {
+		debug!("Not resolving any messages in DM");
+		return Ok(Vec::new());
+	};
+
+	let source_server = source_server_id
+		.to_guild_cached(ctx)
+		.ok_or_else(|| eyre!("Guild {} not cached", source_server_id))?
+		.to_owned();
+
+	let member = source_server.member(&ctx.http, &msg.author).await?;
+
 	let matches = MESSAGE_PATTERN
 		.captures_iter(&msg.content)
 		.map(|capture| capture.extract());
 
 	let mut embeds: Vec<CreateEmbed> = vec![];
 
-	for (url, [_server_id, channel_id, message_id]) in matches {
+	for (url, [server_id, channel_id, message_id]) in matches {
+		if server_id != source_server_id.to_string() {
+			debug!("Not resolving message from other server");
+			continue;
+		}
+
 		trace!("Attempting to resolve message {message_id} from URL {url}");
 
-		let channel = ChannelId::from_str(channel_id)
+		let Ok(Channel::Guild(channel)) = ChannelId::from_str(channel_id)
 			.wrap_err_with(|| format!("Couldn't parse channel ID {channel_id}!"))?
-			.to_channel_cached(ctx.as_ref())
-			.ok_or_else(|| eyre!("Couldn't find Guild Channel from {channel_id}!"))?
-			.to_owned();
+			.to_channel(ctx)
+			.await
+		else {
+			debug!(
+				"Not resolving message in {} as it does not appear to exist",
+				channel_id
+			);
+			continue;
+		};
 
-		let author_can_view = if channel.kind == ChannelType::PublicThread
-			|| channel.kind == ChannelType::PrivateThread
-		{
-			let thread_members = channel
-				.id
-				.get_thread_members(ctx)
-				.await
-				.wrap_err("Couldn't get members from thread!")?;
+		let author_can_view = match channel.kind {
+			ChannelType::Text | ChannelType::News => source_server
+				.user_permissions_in(&channel, member.as_ref())
+				.contains(Permissions::VIEW_CHANNEL),
 
-			thread_members
-				.iter()
-				.any(|member| member.user_id == msg.author.id)
-		} else {
-			channel
-				.members(ctx)
-				.wrap_err_with(|| format!("Couldn't get members for channel {channel_id}!"))?
-				.iter()
-				.any(|member| member.user.id == msg.author.id)
+			ChannelType::PublicThread => {
+				let parent = channel
+					.parent_id
+					.ok_or_else(|| eyre!("Thread {} has no parent", channel.id))?
+					.to_channel_cached(&ctx.cache)
+					.ok_or_else(|| eyre!("Parent channel of {} not cached", channel.id))?;
+
+				source_server
+					.user_permissions_in(&parent, member.as_ref())
+					.contains(Permissions::VIEW_CHANNEL)
+			}
+
+			_ => false,
 		};
 
 		if !author_can_view {
 			debug!("Not resolving message for author who can't see it");
+			continue;
 		}
 
-		let original_message = channel
+		let Ok(original_message) = channel
 			.message(
 				ctx,
 				MessageId::from_str(message_id)
 					.wrap_err_with(|| format!("Couldn't parse message ID {message_id}!"))?,
 			)
 			.await
-			.wrap_err_with(|| {
-				format!("Couldn't get message from ID {message_id} in channel {channel_id}!")
-			})?;
+		else {
+			debug!("No message found in {channel_id} by {message_id}");
+			continue;
+		};
 
 		let author = CreateEmbedAuthor::new(original_message.author.tag()).icon_url(
 			original_message
