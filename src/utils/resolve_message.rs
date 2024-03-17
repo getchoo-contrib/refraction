6455f33688dea6dd5a48824941937dc0cb737a87
seth <getchoo@tuta.io>
2024-03-16 22:31:26 -0400

    utils: factor out message resolution

--- src/utils/resolve_message.rs
+++ src/utils/resolve_message.rs
@@ -4,17 +4,14 @@ use eyre::{eyre, Context as _, Result};
 use log::{debug, trace};
 use once_cell::sync::Lazy;
 use poise::serenity_prelude::{
-	Channel, ChannelId, ChannelType, Colour, Context, CreateEmbed, CreateEmbedAuthor,
-	CreateEmbedFooter, Message, MessageId, Permissions,
+	Cache, CacheHttp, ChannelId, ChannelType, Colour, Context, CreateEmbed, CreateEmbedAuthor,
+	CreateEmbedFooter, GuildChannel, Member, Message, MessageId, Permissions,
 };
 use regex::Regex;
 
-static MESSAGE_PATTERN: Lazy<Regex> = Lazy::new(|| {
-	Regex::new(r"(?:https?:\/\/)?(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?<server_id>\d+)\/(?<channel_id>\d+)\/(?<message_id>\d+)").unwrap()
-});
-
-fn find_first_image(msg: &Message) -> Option<String> {
-	msg.attachments
+fn find_first_image(message: &Message) -> Option<String> {
+	message
+		.attachments
 		.iter()
 		.find(|a| {
 			a.content_type
@@ -25,18 +22,91 @@ fn find_first_image(msg: &Message) -> Option<String> {
 		.map(|res| res.url.clone())
 }
 
-pub async fn resolve(ctx: &Context, msg: &Message) -> Result<Vec<CreateEmbed>> {
-	let Some(source_server_id) = msg.guild_id else {
-		debug!("Not resolving any messages in DM");
-		return Ok(Vec::new());
+async fn member_can_view_channel(
+	ctx: impl CacheHttp + AsRef<Cache>,
+	member: &Member,
+	channel: &GuildChannel,
+) -> Result<bool> {
+	static REQUIRED_PERMISSIONS: Lazy<Permissions> =
+		Lazy::new(|| Permissions::VIEW_CHANNEL | Permissions::READ_MESSAGE_HISTORY);
+
+	let guild = ctx.http().get_guild(channel.guild_id).await?;
+
+	let channel_to_check = match &channel.kind {
+		ChannelType::PublicThread => {
+			let parent_id = channel
+				.parent_id
+				.ok_or_else(|| eyre!("Couldn't get parent of thread {}", channel.id))?;
+			parent_id
+				.to_channel(ctx)
+				.await?
+				.guild()
+				.ok_or_else(|| eyre!("Couldn't get GuildChannel from ChannelID {parent_id}!"))?
+		}
+
+		_ => channel.to_owned(),
 	};
 
-	let source_server = source_server_id
-		.to_guild_cached(ctx)
-		.ok_or_else(|| eyre!("Guild {} not cached", source_server_id))?
-		.to_owned();
+	let can_view = guild
+		.user_permissions_in(&channel_to_check, member)
+		.contains(*REQUIRED_PERMISSIONS);
+	Ok(can_view)
+}
 
-	let member = source_server.member(&ctx.http, &msg.author).await?;
+pub async fn to_embed(
+	ctx: impl CacheHttp + AsRef<Cache>,
+	message: &Message,
+) -> Result<CreateEmbed> {
+	let author = CreateEmbedAuthor::new(message.author.tag()).icon_url(
+		message
+			.author
+			.avatar_url()
+			.unwrap_or_else(|| message.author.default_avatar_url()),
+	);
+
+	let footer = CreateEmbedFooter::new(format!(
+		"#{}",
+		message.channel(ctx).await?.guild().unwrap_or_default().name
+	));
+
+	let mut embed = CreateEmbed::new()
+		.author(author)
+		.color(Colour::BLITZ_BLUE)
+		.timestamp(message.timestamp)
+		.footer(footer)
+		.description(format!(
+			"{}\n\n[Jump to original message]({})",
+			message.content,
+			message.link()
+		));
+
+	if !message.attachments.is_empty() {
+		embed = embed.fields(message.attachments.iter().map(|a| {
+			(
+				"Attachments".to_string(),
+				format!("[{}]({})", a.filename, a.url),
+				false,
+			)
+		}));
+
+		if let Some(image) = find_first_image(message) {
+			embed = embed.image(image);
+		}
+	}
+
+	Ok(embed)
+}
+
+pub async fn from_message(ctx: &Context, msg: &Message) -> Result<Vec<CreateEmbed>> {
+	static MESSAGE_PATTERN: Lazy<Regex> = Lazy::new(|| {
+		Regex::new(r"(?:https?:\/\/)?(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?<server_id>\d+)\/(?<channel_id>\d+)\/(?<message_id>\d+)").unwrap()
+	});
+
+	let Some(guild_id) = msg.guild_id else {
+		debug!("Not resolving message in DM");
+		return Ok(Vec::new());
+	};
+	let author = guild_id.member(ctx, msg.author.id).await?;
 
 	let matches = MESSAGE_PATTERN
 		.captures_iter(&msg.content)
@@ -44,95 +114,35 @@ pub async fn resolve(ctx: &Context, msg: &Message) -> Result<Vec<CreateEmbed>> {
 
 	let mut embeds: Vec<CreateEmbed> = vec![];
 
-	for (url, [server_id, channel_id, message_id]) in matches {
-		if server_id != source_server_id.to_string() {
+	for (url, [target_guild_id, target_channel_id, target_message_id]) in matches {
+		if target_guild_id != guild_id.to_string() {
 			debug!("Not resolving message from other server");
 			continue;
 		}
+		trace!("Attempting to resolve message {target_message_id} from URL {url}");
 
-		trace!("Attempting to resolve message {message_id} from URL {url}");
-
-		let Ok(Channel::Guild(channel)) = ChannelId::from_str(channel_id)
-			.wrap_err_with(|| format!("Couldn't parse channel ID {channel_id}!"))?
+		let target_channel = ChannelId::from_str(target_channel_id)?
 			.to_channel(ctx)
-			.await
-		else {
-			debug!(
-				"Not resolving message in {} as it does not appear to exist",
-				channel_id
-			);
-			continue;
-		};
+			.await?
+			.guild()
+			.ok_or_else(|| {
+				eyre!("Couldn't find GuildChannel from ChannelId {target_channel_id}!")
+			})?;
 
-		let author_can_view = match channel.kind {
-			ChannelType::Text | ChannelType::News => source_server
-				.user_permissions_in(&channel, member.as_ref())
-				.contains(Permissions::VIEW_CHANNEL),
-
-			ChannelType::PublicThread => {
-				let parent = channel
-					.parent_id
-					.ok_or_else(|| eyre!("Thread {} has no parent", channel.id))?
-					.to_channel_cached(&ctx.cache)
-					.ok_or_else(|| eyre!("Parent channel of {} not cached", channel.id))?;
-
-				source_server
-					.user_permissions_in(&parent, member.as_ref())
-					.contains(Permissions::VIEW_CHANNEL)
-			}
-
-			_ => false,
-		};
-
-		if !author_can_view {
+		if !member_can_view_channel(ctx, &author, &target_channel).await? {
 			debug!("Not resolving message for author who can't see it");
 			continue;
 		}
 
-		let Ok(original_message) = channel
-			.message(
-				ctx,
-				MessageId::from_str(message_id)
-					.wrap_err_with(|| format!("Couldn't parse message ID {message_id}!"))?,
-			)
+		let target_message_id = MessageId::from_str(target_message_id)?;
+		let target_message = target_channel
+			.message(ctx, target_message_id)
 			.await
-		else {
-			debug!("No message found in {channel_id} by {message_id}");
-			continue;
-		};
+			.wrap_err_with(|| {
+				eyre!("Couldn't find channel message from ID {target_message_id}!")
+			})?;
 
-		let author = CreateEmbedAuthor::new(original_message.author.tag()).icon_url(
-			original_message
-				.author
-				.avatar_url()
-				.unwrap_or_else(|| original_message.author.default_avatar_url()),
-		);
-		let footer = CreateEmbedFooter::new(format!("#{}", channel.name));
-
-		let mut embed = CreateEmbed::new()
-			.author(author)
-			.color(Colour::BLITZ_BLUE)
-			.timestamp(original_message.timestamp)
-			.footer(footer)
-			.description(format!(
-				"{}\n\n[Jump to original message]({})",
-				original_message.content,
-				original_message.link()
-			));
-
-		if !original_message.attachments.is_empty() {
-			embed = embed.fields(original_message.attachments.iter().map(|a| {
-				(
-					"Attachments".to_string(),
-					format!("[{}]({})", a.filename, a.url),
-					false,
-				)
-			}));
-
-			if let Some(image) = find_first_image(msg) {
-				embed = embed.image(image);
-			}
-		}
+		let embed = to_embed(ctx, &target_message).await?;
 
 		embeds.push(embed);
 	}
